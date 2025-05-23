074565c1a82895d4cbad66d66dccb3baa6d9f849
MikhailKuliev <mkh_3112@mail.ru>
2025-05-23 20:27:18 +0300

    Внес изменение в файл

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         BookAction action = new BookAction() {
             @Override
             public void performAction(Book book) {
-                System.out.println("выполнения действий: " + book.getInfo() );
+                System.out.println("ыполнения действий: " + book.getInfo() );
 
             }
         };
