5e5a7c7186baff7d59b8d23773688645b627c9e5
MikhailKuliev <mkh_3112@mail.ru>
2025-05-23 20:00:17 +0300

    поменял класс мейн

--- src/Library.java
+++ src/Library.java
@@ -18,17 +18,3 @@ public class Library {
 }
 
 
-class LibraryMember {
-    private String name;
-    private int MembershipId;
-    public LibraryMember(String name, int MembershipId) {
-        this.name = name;
-        this.MembershipId = MembershipId;
-
-    }
-    public String getMemberInfo() {
-        return " Название : " + name + ", Id: " + MembershipId;
-
-    }
-}
-
